984da7f72803bfb472fe804f4292f9c622da44aa
Delilah-Lawes03 <118970322+Delilah-Lawes03@users.noreply.github.com>
2024-07-31 14:46:56 +0100

    update index.html, script.js and style.css

--- src/script.js
+++ src/script.js
@@ -1,44 +1,58 @@
-const searchForm = document.querySelector(".search-input");
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-document.querySelector("form").addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  let apiKey = "t422032f5oa11113f5b0fbcadff3ac4a";
-  let cityInput = searchForm.value;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayWeatherData);
-});
-
-function displayWeatherData(response) {
-  let temperature = Math.round(response.data.temperature.current);
-  let city = response.data.city;
-  let description = response.data.condition.description
-    .toLowerCase()
-    .replace(/^\w/, (c) => c.toUpperCase()); // Convert to sentence case
-  let humidity = response.data.temperature.humidity;
-  let wind = response.data.wind.speed;
-  const timestamp = response.data.time;
-  const date = new Date(timestamp * 1000);
-
-  const day = date.toLocaleDateString("en-US", { weekday: "long" });
-  const time = date.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-  });
-
-  let iconUrl = response.data.condition.icon_url;
-  let icon = response.data.condition.icon;
-
-  document.getElementById("city").textContent = city;
-  document.getElementById("temperature").textContent = temperature;
-  document.querySelector(".current-day").textContent = day;
-  document.querySelector(".current-time").textContent = time;
-  document.querySelector(".description").textContent = description;
-  document.querySelector(".humidity").textContent = humidity + "%";
-  document.querySelector(".wind").textContent = wind + "km/h";
-
-  document.querySelector(".current-temperature-icon").src = iconUrl;
-  document.querySelector(".current-temperature-icon").alt = icon;
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiKey = "t422032f5oa11113f5b0fbcadff3ac4a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
